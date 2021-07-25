4f51af00e6b3bf78aa4c8ce963021b500abb5e32
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-07-25 10:13:29 -0300

    Primeiro passo do teste funcional: enxergar título

--- tdd-python-book/functional_tests.py
+++ tdd-python-book/functional_tests.py
@@ -1,7 +1,9 @@
+import unittest
 from selenium import webdriver
 
 # browser = webdriver.Chrome()
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+# Ao acessar a aplicação o usuário vê o título "Lista de Tarefas"
+browser.get('http://localhost:8000')
+assert "Lista de Tarefas" in browser.title
