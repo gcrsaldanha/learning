d5b7ce80e47dc98c3117c613505dad263f2c801a
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-07-25 11:05:43 -0300

    Configura test suit

--- tdd-python-book/functional_tests.py
+++ tdd-python-book/functional_tests.py
@@ -1,9 +1,21 @@
-import unittest
+from unittest import TestCase, main
 from selenium import webdriver
 
-# browser = webdriver.Chrome()
-browser = webdriver.Firefox()
 
-# Ao acessar a aplicação o usuário vê o título "Lista de Tarefas"
-browser.get('http://localhost:8000')
-assert "Lista de Tarefas" in browser.title
+class TestPaginaInicial(TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_usuario_visualiza_o_titulo_da_pagina(self):
+        # Ao acessar a aplicação o usuário vê o título "Lista de Tarefas"
+        self.browser.get('http://localhost:8000')
+        # assert 'Lista de Tarefas' in self.browser.title, '"Lista de Tarefas" não encontrada no título'
+        # Outra opção:
+        self.assertIn('Lista de Tarefas', self.browser.title)
+
+
+if __name__ == '__main__':
+    main()
